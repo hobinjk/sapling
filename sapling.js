ed95f1fbb7bfc5028f09c975b7a7e82c4a38bea1
James Hobin <hobinjk@mit.edu>
2016-05-24 01:03:30 -0400

    Implement completely out-of-order imports and exports

--- sapling.js
+++ sapling.js
@@ -1,76 +1,138 @@
 var recast = require('recast');
+var b = recast.types.builders;
 var fs = require('fs');
 
 var entryFileName = process.argv[2];
 
-var fileInfo = {};
-addData(fileInfo, process.argv[2]);
+var processedFiles = {};
+var topLevelAst = readAst(entryFileName);
+topLevelAst.program.body = addData(entryFileName, []);
+console.log(recast.print(topLevelAst).code);
 
-function addData(fileInfo, fileName) {
-  var entrySource = fs.readFileSync(entryFileName, {encoding: 'utf8'});
-  var ast = recast.parse(entrySource);
-  var newInfo = processProgram(ast.program);
-  fileInfo[fileName] = newInfo;
-
-  for (var importName in newInfo.imports) {
-    var importPath = resolvePath(fileName, newInfo.imports[importName].source);
-    if (typeof(fileInfo[importPath]) === 'undefined') {
-      addData(fileInfo, importPath);
-    }
-  }
+function readAst(fileName) {
+  var source = fs.readFileSync(fileName, {encoding: 'utf8'});
+  return recast.parse(source);
 }
 
-function processProgram(program) {
-  var exports = {};
-  var imports = {};
+function addData(fileName, output) {
+  processedFiles[fileName] = true;
+  var ast = readAst(fileName);
+  var processOutput = processProgram(fileName, ast.program);
+
+  // Combine newAst with topLevelAst
+  output = output.concat(processOutput.body);
+
+  processOutput.imports.forEach(function(importSource) {
+    var importPath = resolvePath(fileName, importSource);
+    if (!processedFiles[importPath]) {
+      output = addData(importPath, output);
+    }
+  });
+
+  return output;
+}
+
+function processProgram(fileName, program) {
+  var imports = [];
+  var newBody = [];
   program.body.forEach(function(node) {
     switch (node.type) {
     case 'ExportDefaultDeclaration':
-      exports['default'] = node.declaration.name;
+
+      newBody = newBody.concat(makeAssignment(fileName, 'default', node.declaration));
       break;
     case 'ExportNamedDeclaration':
       if (node.declaration) {
-        var name = node.declaration.id.name;
-        exports[name] = node.declaration;
+        newBody = newBody.concat(makeExport(fileName, node.declaration));
       } else if (node.specifiers) {
-        // TODO support export {varA as exportedA}
         node.specifiers.forEach(function(spec) {
-          var name = spec.exported.name;
-          exports[name] = spec.local.name;
+          newBody = newBody.concat(makeAssignment(fileName, spec.exported.name, spec.local));
         });
       }
       break;
     case 'ImportDeclaration':
       node.specifiers.forEach(function(spec) {
+        var localName = spec.local.name;
+        var source = node.source.value;
+        imports.push(source);
         switch (spec.type) {
           case 'ImportDefaultSpecifier':
-            imports[spec.local.name] = {
-              source: node.source.value,
-              imported: 'default'
-            }
+            newBody = newBody.concat(makeImportNamed(localName, source, 'default'));
             break;
           case 'ImportSpecifier':
-            imports[spec.local.name] = {
-              source: node.source.value,
-              imported: spec.local.name
-            };
+            newBody = newBody.concat(makeImportNamed(localName, source, localName));
             break;
         }
       });
+      break;
+    default:
+      newBody.push(node);
+      break;
     }
   });
 
-  console.log('exports', exports);
-  console.log('imports', imports);
-
   return {
-    exports: exports,
+    body: newBody,
     imports: imports
   };
+}
+
+// Export: scope[source].name = local
+// Import: local = scope[source].name
+
+function makeExport(fileName, declaration) {
+  switch (declaration.type) {
+  case 'VariableDeclaration':
+    var output = [declaration];
+    declaration.declarations.forEach(function(decl) {
+      var declName = declarationName(decl);
+      output = output.concat(makeAssignment(fileName, declName, decl.init || decl.id));
+    });
+    return output;
+  case 'FunctionDeclaration':
+    return makeAssignment(fileName, declarationName(declaration), declaration);
+  }
+}
+
+function makeAssignable(declaration) {
+  if (declaration.type === 'FunctionDeclaration') {
+    return makeFunctionExpression(declaration);
+  }
+  return declaration;
+}
+
+function makeFunctionExpression(functionDeclaration) {
+  // Might drop generator status or anything else weird
+  return b.functionExpression(
+    functionDeclaration.id,
+    functionDeclaration.params,
+    functionDeclaration.body
+  );
+}
+
+function makeAssignment(fileName, exportName, local) {
+  return [b.assignmentStatement('=', makeScope(fileName, exportName), makeAssignable(local))];
+}
+
+function declarationName(declaration) {
+  return declaration.id.name;
+}
+
+function makeScope(fileName, exportName) {
+  return b.memberExpression(
+    b.memberExpression(
+      b.identifier('scope'),
+      b.literal(fileName)),
+    b.literal(exportName));
+}
+
+function makeImportNamed(localName, source, exportName) {
+  return b.variableDeclaration('var', [
+    b.variableDeclarator(
+      b.identifier(localName), makeScope(source, exportName))
+  ]);
 }
 
 function resolvePath(originatingFileName, path) {
   return path;
 }
-
-console.log(JSON.stringify(fileInfo));
